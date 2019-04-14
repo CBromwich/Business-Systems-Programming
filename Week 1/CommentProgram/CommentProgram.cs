f0905dfccd1af4fb44f64d78525887b9929d85f7
Cody Bromwich <cjbromwich@gmail.com>
2019-04-14 04:05:02 -0400

    Initial commit of CommentProgram

---
+++ Week 1/CommentProgram/CommentProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class CommentProgram { 
+    /*
+    This is a block comment
+     */ 
+    static void Main(string[] args) {
+        // This is a single line comment
+        Console.WriteLine("Hello World!");
+    }
+}
